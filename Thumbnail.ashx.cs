921ba4f33a17de5c2375c509479166cbb968188b
Slava <slavapas13@gmail.com>
2015-02-26 11:24:49 +0200

    jQueryFileUploadNet

---
+++ Thumbnail.ashx.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Web;
+
+namespace jQueryUploadTest {
+	public class Thumbnail : IHttpHandler {
+
+		public void ProcessRequest (HttpContext context)
+		{
+		    string fullName = Path.Combine(context.Request.PhysicalApplicationPath, "images/default_thumb.jpg");
+			context.Response.ContentType = "image/jpg";
+
+            context.Response.WriteFile(fullName);
+		}
+
+		public bool IsReusable { get { return false; } }
+	}
+}
